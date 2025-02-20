6dfca29d3286fe75fd9dcd2300f4cd0717cbce8c
Dhimitri Sinjari <jili62ry@localhost.localdomain>
2025-02-20 11:00:42 +0100

    added more functions until mAP, next non max supression

--- src_code/task_utils/evaluation.py
+++ src_code/task_utils/evaluation.py
@@ -1,6 +1,10 @@
 # source https://gist.github.com/tuxedocat/fb024dfa36648797084d
 
 import numpy as np
+import torch
+from src_code.data_utils.dataset_utils import get_dataloader
+from src_code.model_utils.utils import find_IoU
+
 
 def levenshtein(x, y):
     
@@ -68,4 +72,73 @@ def edit_score(model, dataloader):
             edit_distances.append(edit_dist)
 
     return edit_distances
+
+
+# next compute precision & recall for a single class
+# it takes as arguments detections (list of predicted boxes)
+#   list of ground truth boxes and IoU threshold.
+# returns precision list and recall list to be used to compute AP
+
+def compute_precision_recall(detections, ground_truths, iou_threshold=0.5):
+
+    detections = sorted(detections, key=lambda x: x[0], reverse=True)  # Sort by confidence score
+    true_positives = np.zeros(len(detections))
+    false_positives = np.zeros(len(detections))
+    total_gt = len(ground_truths)
+    matched_gts = set()
+    
+    for i, (confidence, pred_box) in enumerate(detections):
+        best_iou = 0
+        best_gt_idx = -1
+
+        for j, gt_box in enumerate(ground_truths):
+            iou = compute_iou(pred_box, gt_box)
+            if iou > best_iou:
+                best_iou = iou
+                best_gt_idx = j
+
+        if best_iou >= iou_threshold and best_gt_idx not in matched_gts:
+            true_positives[i] = 1
+            matched_gts.add(best_gt_idx)
+        else:
+            false_positives[i] = 1
+
+    cumulative_true_positives = np.cumsum(true_positives)
+    cumulative_false_positives = np.cumsum(false_positives)
+    precision = cumulative_true_positives / (cumulative_true_positives + cumulative_false_positives + 1e-8)
+    recall = cumulative_true_positives / total_gt if total_gt else np.zeros(len(detections))
+
+    return precision, recall
+
+
+def compute_average_precision(precision, recall):
+
+    recall = np.concatenate(([0], recall, [1]))
+    precision = np.concatenate(([0], precision, [0]))
+
+    for i in range(len(precision) - 1, 0, -1):
+        precision[i - 1] = max(precision[i - 1], precision[i])
+
+    indices = np.where(recall[1:] != recall[:-1])[0]
+    average_precision = np.sum((recall[indices + 1] - recall[indices]) * precision[indices + 1])
+
+    return average_precision
+
+
+# compute mAP
+
+def compute_map(predictions, ground_truths, num_classes):
+
+    aps = []
+
+    for class_id in range(num_classes):
+        if class_id not in predictions:
+            continue
+
+        precision, recall = compute_precision_recall(predictions[class_id], ground_truths.get(class_id, []))
+        average_precision = compute_average_precision(precision, recall)
+        aps.append(ap)
+
+    return np.mean(aps) if aps else 0
+
 
